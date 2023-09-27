c837fb7e403cad6aa6891de57b7b855bac3ba238
Martha9876 <144279647+Martha9876@users.noreply.github.com>
2023-09-27 21:23:45 +0100

    Add files via upload

---
+++ client/src/pages/Page111/Page111.js
@@ -0,0 +1,57 @@
+/** @format */
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useContext, useEffect, useState } from "react";
+import "./Page111.css";
+import axios from "axios";
+import { Questioncontext } from "../../context/Context";
+
+const Page1 = () => {
+  const [data, setdata] = useState([]);
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:3001/getvalues");
+    setdata(response.data);
+    console.log(data);
+  };
+  useEffect(() => {
+    
+    fetchData();
+
+  }, []);
+  const Globalstate = useContext(Questioncontext);
+  const dispatch = Globalstate.dispatch;
+  console.log(Globalstate);
+  return (
+    
+    <div className="home">
+      
+      {data.map((item, index) => {
+        item.quantity = 1;
+        
+        return (
+          <div className="container mt-5">
+          <div className="row">
+          <div className="card" key={index}>
+        
+        
+           <h1 className="Heading"> {item.heading}</h1>
+            
+           <h3 className="subHeading"> {item.subheading}</h3>
+
+            <a href="/page21211" className="btn btn-primary mb-4" onClick={() => dispatch({ type: "text", payload: item })}>
+              {item.butn}
+            </a>
+            
+          </div>
+          </div>
+          
+          </div>
+          
+        );
+      })}
+      
+    </div>
+  );
+};
+
+export default Page1;
